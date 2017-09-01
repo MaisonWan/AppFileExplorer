f8cd45eb958142578e01ef54a8f0724ee08bd6b3
wanlipeng <wanlipeng.rd@bytedance.com>
2017-09-01 16:43:04 +0800

    增加显示宿主app的名字，图标等。

---
+++ FileExplorer/src/main/java/com/domker/app/explorer/fragment/AppInfoFragment.kt
@@ -0,0 +1,22 @@
+package com.domker.app.explorer.fragment
+
+import android.content.Context
+import android.view.View
+import com.domker.app.explorer.R
+
+/**
+ * App信息
+ * Created by wanlipeng on 2017/9/1.
+ */
+class AppInfoFragment : BaseFragment() {
+
+    override fun init(context: Context, view: View) {
+    }
+
+    override fun onShown(context: Context) {
+    }
+
+    override fun initLayoutId(): Int = R.layout.fe_app_info_layout
+
+
+}
